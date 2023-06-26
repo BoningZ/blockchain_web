e86ad31524ef0f084566bfe5f9b4c472757137d9
BoningZ <243208074@qq.com>
2023-06-26 23:39:43 +0800

    1

---
+++ fabric-test/src/main/java/com/sansec/fabrictest/FabricTestApplication.java
@@ -0,0 +1,13 @@
+package com.sansec.fabrictest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FabricTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FabricTestApplication.class, args);
+    }
+
+}
---
+++ fabric-test/src/test/java/com/sansec/fabrictest/FabricTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sansec.fabrictest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FabricTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
