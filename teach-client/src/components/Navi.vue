cc802ee46ece822f1cc3c198c510422bd1459995
BoningZ <243208074@qq.com>
2023-07-12 12:09:59 +0800

    logout attached to navi

--- teach-client/src/components/Navi.vue
+++ teach-client/src/components/Navi.vue
@@ -1,13 +1,16 @@
 <template>
-  <div class="navi">
+  <div >
     <el-menu
         :default-active="$route.path"
         class="el-menu-demo"
         mode="horizontal"
-        router
-    >
+        :ellipsis="false"
+        router>
       <el-menu-item index="/">首页</el-menu-item>
-      <el-menu-item class="menu-item" v-for="(v,i) in list" :key="i" :index="'/'+v.name">{{v.title}}</el-menu-item>
+      <el-menu-item  v-for="(v,i) in list" :key="i" :index="'/'+v.name">{{v.title}}</el-menu-item>
+
+      <div class="flex-grow" />
+      <el-menu-item><el-button type="danger" @click='logout'>登出</el-button></el-menu-item>
     </el-menu>
     <router-view></router-view>
   </div>
@@ -15,6 +18,7 @@
 
 <script>
 import {getMenuList} from '@/service/genServ.js'
+import router from "@/router";
 export default {
   name: 'MyNavi',
   components: {
@@ -28,6 +32,10 @@ export default {
     }
   },
   methods:{
+    logout() {
+      this.$store.commit('logout')
+      router.push('/login')
+    },
     setMenu() {
       var self = this
       getMenuList()
@@ -39,3 +47,14 @@ export default {
   }
 }
 </script>
+<style>
+.el-menu {
+  align-items: center; /* 垂直居中对齐 */
+}
+
+
+.flex-grow {
+  flex-grow: 1;
+}
+
+</style>
