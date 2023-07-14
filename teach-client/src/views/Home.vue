ec85e8a82444a94177260a72653a2de7754dfebb
BoningZ <243208074@qq.com>
2023-07-14 11:17:37 +0800

    home modify

--- teach-client/src/views/Home.vue
+++ teach-client/src/views/Home.vue
@@ -2,17 +2,15 @@ import
 <template>
   <router-view>
   <div class="home">
-    <el-card  style="width: 40%;margin-left: 30%;margin-top: 10px;margin-bottom: 20px" >
-      <template #header>
-        <el-row>
-          <el-col :span="10">
-            <el-text type="primary">{{infoForm.username}}</el-text>
-          </el-col>
-          <el-col :span="14">
-            <el-tag type="success">{{infoForm.type}}</el-tag>
-          </el-col>
-        </el-row>
-      </template>
+    <el-card  style="width: 20%;margin-left: 40%;margin-top: 10px;margin-bottom: 20px" >
+      <el-row>
+          <el-text>用户名：</el-text>
+          <el-text type="primary">{{infoForm.username}}</el-text>
+      </el-row>
+      <el-row>
+          <el-text>角色：</el-text>
+          <el-tag type="success">{{infoForm.type}}</el-tag>
+      </el-row>
       <el-row>
         <el-text>{{"姓名："+infoForm.name}}</el-text>
       </el-row>
@@ -26,23 +24,21 @@ import
       <template #header>
         <el-row>
           <el-col :span="4">
-            <el-text type="success">{{"权限 #"+(index+1)}}</el-text>
+            <el-text type="success">{{"权限 "+(index+1)}}</el-text>
           </el-col>
           <el-col :span="12">
-            <el-text type="success" size="large">{{item.name}}</el-text>
+            <el-text type="success" size="large">{{"权限名："+item.name}}</el-text>
           </el-col>
           <el-col :span="8">
             <el-text type="info">{{"创建人："+item.admin.name}}</el-text>
-            &nbsp;
-            <el-tag type="warning">{{"#"+item.admin.aid}}</el-tag>
           </el-col>
         </el-row>
       </template>
       <el-row>
-        <el-col :span="6">
+        <el-col :span="2">
           <el-text type="info">组成：</el-text>
         </el-col>
-        <el-col :span="18">
+        <el-col :span="22" style="  display: flex; justify-content: flex-start;">
           <el-tag v-for="type in item.rightTypes" type="primary" v-bind:key="type">{{type.label}}</el-tag>
         </el-col>
       </el-row>
@@ -75,6 +71,7 @@ export default {
     getBasicInfo().then(res=>{
       this.infoForm=res.data
       if(this.infoForm.type==="成员"){
+        this.infoForm.type="普通用户"
         getMyRights().then(res2=>{
           this.rightList=res2.data.data
         })
